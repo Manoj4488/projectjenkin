24a8660ac8d3ba33b342e69c3cf3cb44878bde55
Manoj4488 <manojkarumuri96@gmail.com>
2021-09-21 23:11:24 +0530

    commiting for firsttime

---
+++ crmcastseleniumframes/src/test/java/com/crm/contactss/ContactsTest.java
@@ -0,0 +1,19 @@
+package com.crm.contactss;
+
+import org.testng.annotations.Test;
+
+public class ContactsTest {
+	@Test
+	public void createcontactTest() {
+		System.out.println("contact created");
+	}
+	@Test
+	public void createcontactwithargTest() {
+		System.out.println("contactwitharg is created");
+	}
+	@Test
+	public void deletecontactTest() {
+		System.out.println("contact deleted");
+	}
+
+}
