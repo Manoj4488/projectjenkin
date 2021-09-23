076ae08eb9167c7fbc5d86657e26d62e870fa5e8
Manoj4488 <manojkarumuri96@gmail.com>
2021-09-23 14:44:06 +0530

    parameters added

--- crmcastseleniumframes/src/test/java/com/crm/contactss/ContactsTest.java
+++ crmcastseleniumframes/src/test/java/com/crm/contactss/ContactsTest.java
@@ -3,6 +3,10 @@ package com.crm.contactss;
 import org.testng.annotations.Test;
 
 public class ContactsTest {
+	String bw=System.getProperty("browser");
+	String urll=System.getProperty("url");
+	String env=System.getProperty("env");
+	
 	@Test(groups="SmokeTest")
 	public void createcontactTest() {
 		System.out.println("contact created");
@@ -18,6 +22,13 @@ public class ContactsTest {
 	@Test(groups="SmokeTest")
 	public void addcontactTest() {
 		System.out.println("contact added pollscm");
-	}
-
+		}
+	@Test(groups="SmokeTest")
+	public void parametersTest() {
+		System.out.println(bw);
+		System.out.println(urll);
+		System.out.println(env);
+		
+		}
+      
 }
