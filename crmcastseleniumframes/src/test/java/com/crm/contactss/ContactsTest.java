93d1bca925dc71c1773efb2b3ca265fa7c4b8362
Manoj4488 <manojkarumuri96@gmail.com>
2021-09-23 15:00:04 +0530

    parameter again upd

--- crmcastseleniumframes/src/test/java/com/crm/contactss/ContactsTest.java
+++ crmcastseleniumframes/src/test/java/com/crm/contactss/ContactsTest.java
@@ -3,9 +3,6 @@ package com.crm.contactss;
 import org.testng.annotations.Test;
 
 public class ContactsTest {
-	String bw=System.getProperty("browser");
-	String urll=System.getProperty("url");
-	String env=System.getProperty("env");
 	
 	@Test(groups="SmokeTest")
 	public void createcontactTest() {
@@ -25,9 +22,13 @@ public class ContactsTest {
 		}
 	@Test(groups="SmokeTest")
 	public void parametersTest() {
-		System.out.println(bw);
-		System.out.println(urll);
-		System.out.println(env);
+		String bw=System.getProperty("browser");
+		String urll=System.getProperty("url");
+		String envv=System.getProperty("env");
+		
+		System.out.println("browser======"+bw);
+		System.out.println("url======"+urll);
+		System.out.println("env======"+envv);
 		
 		}
       
